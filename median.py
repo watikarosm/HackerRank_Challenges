48b8478fb82c5e10940458cd37bdfe23c5ddab80
WATIS EKTHUVAPRANEE <45465335+watikarosm@users.noreply.github.com>
2022-02-06 17:36:09 -0500

    Update 1
    Fixed for an empty set case

--- median.py
+++ median.py
@@ -1,31 +1,35 @@
 import numpy as np
 import random
 
+
 def median(arr):
     print(arr)
     arr.sort()
     print(arr)
     j = len(arr)
     print(j)
-    if j % 2 == 0:  # odd case
+    if j == 0:
+        print("This is an empty set.")
+    elif j % 2 == 0:  # even case
         k = int((j + 1) / 2)
-        #        print(k)
-        #        l = (array[k-1] + array[k])/2
         print((arr[k - 1] + arr[k]) / 2, " is the median.")
-    else:  # even case
+    else:  # odd case
         k = int(j/2)
         print(arr[k], " is the median")
+
 
 if __name__ == '__main__':
     array = [1, 3, 2, 5, 4, 7, 6]
     array1 = [2, 3, 8, 2, 4, 1, 5, 7]
     array2 = []
-    numSet = random.randint(1, 9)
+    array3 = []
+    numSet = random.randint(0, 50000)
     for i in range(numSet+1):
-        j = random.randint(0, 99)
-        array2.append(j)
+        h = random.randint(-9999, 9999)
+        array2.append(h)
     print(array2)
     median(array)
     median(array1)
     median(array2)
+    median(array3)
 

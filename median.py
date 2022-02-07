ef342c807dbdacdec1f967d8811a50dc551047aa
WATIS EKTHUVAPRANEE <45465335+watikarosm@users.noreply.github.com>
2022-02-07 11:12:40 -0500

    Update 1.3
    In the HackerRank, it needs you to return the value after computation.  It took me a while to figure out what it is asking for.

--- median.py
+++ median.py
@@ -1,3 +1,4 @@
+import numpy as np
 import random
 
 
@@ -9,12 +10,15 @@ def median(arr):
     print(j)
     if j == 0:
         print("This is an empty set.")
+        return 0
     elif j % 2 == 0:  # even case
         k = int((j + 1) / 2)
         print((arr[k - 1] + arr[k]) / 2, " is the median.")
+        return (arr[k - 1] + arr[k]) / 2
     else:  # odd case
         k = int(j/2)
         print(arr[k], " is the median")
+        return arr[k]
 
 
 if __name__ == '__main__':
@@ -22,13 +26,19 @@ if __name__ == '__main__':
     array1 = [2, 3, 8, 2, 4, 1, 5, 7]
     array2 = []
     array3 = []
+    array4 = []
     numSet = random.randint(0, 50000)   # creating a random size of the set from 0 to 50000
     for i in range(numSet+1):
         h = random.randint(-9999, 9999)     # get a random value from -9999 to 9999
         array2.append(h)                # add the value above in to the array
+    numSet = random.randint(0, 50000)   # creating a random size of the set from 0 to 50000
+    for i in range(numSet+1):
+        h = random.randint(-9999, 9999)     # get a random value from -9999 to 9999
+        array4.append(h)                # add the value above in to the array
     print(array2)
     median(array)               # case 0: small set with fixed values
     median(array1)              # case 1: small set with fixed and repeated values
     median(array2)              # case 2: random size set with random values
     median(array3)              # case 4: empty set
-
+    answer = median(array4)
+    print("The answer is ", answer)
